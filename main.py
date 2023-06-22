74cf09790aae73b55c26f86abd5c451c0444dac7
BasicCorruption <saytleader@outlook.com>
2023-06-21 22:17:54 -0400

    Day 1

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    char1Name = "John"
+    char2Name = "Jane"
+    char1Age = 20
+    char2AgeDiff = 5
+
+    print(char1Name + " is " + str(char1Age))
+    print(char2Name + " is " + str(char2AgeDiff) + " years older than " + char1Name)
+    print(char2Name + " is " + str(char1Age + char2AgeDiff))
+
+
+if __name__ == '__main__':
+    main()
+
+# https://www.youtube.com/watch?v=rfscVS0vtbw
+# left off at 26:48
