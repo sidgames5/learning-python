d43c3a503a46687f1207b6e4a10a52704f57d92d
BasicCorruption <saytleader@outlook.com>
2023-06-26 15:18:55 -0400

    Day 2

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-def main():
-    char1Name = "John"
-    char2Name = "Jane"
-    char1Age = 20
-    char2AgeDiff = 5
-
-    print(char1Name + " is " + str(char1Age))
-    print(char2Name + " is " + str(char2AgeDiff) + " years older than " + char1Name)
-    print(char2Name + " is " + str(char1Age + char2AgeDiff))
-
-
-if __name__ == '__main__':
-    main()
-
 # https://www.youtube.com/watch?v=rfscVS0vtbw
-# left off at 26:48
+# left off at 58:01
+
+num1 = float(input("First number: "))
+num2 = float(input("Second number: "))
+print(num1 + num2)
