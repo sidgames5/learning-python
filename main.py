6e8b7f221c7bd7a2a182a049d187f8b850880a48
SidGames5 <saytleader@outlook.com>
2023-12-04 19:32:02 -0500

    day 9

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw
-# left off at 2:47:13
+# left off at 2:52:41
+
+number_grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [0]]
 
 
-def raise_to_power(base, power):
-    result = 1
-    for i in range(power):
-        result = result * base
-    print(result)
-
-
-raise_to_power(2, 3)
+for row in number_grid:
+    for col in row:
+        print(col)
