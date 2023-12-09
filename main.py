59c33f8ebb9238975eb15749845500f69c69b0c9
SidGames5 <saytleader@outlook.com>
2023-12-09 11:09:24 -0500

    day 14

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw
-# left off at 3:57:37
+# left off at 4:08:28
 
-from Student import Student
+from Question import Question
 
-student1 = Student("Dave", [93, 88, 99, 100, 81])
-print(student1.name)
-print(student1.gpa)
+
+question_prompts = [
+    "What color are apples?\n(a) Red\n(b) Green\n(c) Blue\n\n",
+    "What color are bananas?\n(a) Black\n(b) Yellow\n(c) Orange\n\n",
+    "What color are strawberries?\n(a) Red\n(b) Yellow\n(c) White\n\n",
+]
+
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "b"),
+    Question(question_prompts[2], "a"),
+]
+
+
+def run_test(questions: list[Question]):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print(f"Score: {str(score)} / {str(len(questions))}")
+    percentage = (score / len(questions)) * 100
+    if percentage >= 90:
+        print("A")
+    elif percentage >= 80:
+        print("B")
+    elif percentage >= 70:
+        print("C")
+    elif percentage >= 65:
+        print("D")
+    else:
+        print("F")
+
+
+run_test(questions)
