3058c1d853521a503cc290061378c21a9f754b53
SidGames5 <saytleader@outlook.com>
2023-12-11 17:07:27 -0500

    day 16

---
+++ ItalianChef.py
@@ -0,0 +1,6 @@
+from Chef import Chef
+
+
+class ItalianChef(Chef):
+    def make_pizza(self):
+        print("The chef makes pizza")
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw
-# left off at 4:12:37
 
-from Student import Student
+from Chef import Chef
+from ItalianChef import ItalianChef
 
-student1 = Student("John", [93, 89])
-student2 = Student("Bob", [82, 99])
+myChef = Chef()
+myChef.make_chicken()
 
-print(student1.getGpa())
-print(student2.getGpa())
+italianChef = ItalianChef()
+italianChef.make_pizza()
