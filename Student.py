fe789b6d0935c4391dbb66f1c2ad3881c623085c
SidGames5 <saytleader@outlook.com>
2023-12-10 20:15:20 -0500

    day 15

--- Student.py
+++ Student.py
@@ -5,9 +5,11 @@ class Student:
     def __init__(self, name, grades: list):
         self.name = name
         self.grades = grades
+
+    def getGpa(self):
         gpa = 0
-        for grade in grades:
+        for grade in self.grades:
             gpa += grade
-        gpa = gpa / len(grades)
+        gpa = gpa / len(self.grades)
         gpa = gpa / 25
-        self.gpa = gpa
+        return gpa
--- main.py
+++ main.py
@@ -1,40 +1,10 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw
-# left off at 4:08:28
+# left off at 4:12:37
 
-from Question import Question
+from Student import Student
 
+student1 = Student("John", [93, 89])
+student2 = Student("Bob", [82, 99])
 
-question_prompts = [
-    "What color are apples?\n(a) Red\n(b) Green\n(c) Blue\n\n",
-    "What color are bananas?\n(a) Black\n(b) Yellow\n(c) Orange\n\n",
-    "What color are strawberries?\n(a) Red\n(b) Yellow\n(c) White\n\n",
-]
-
-questions = [
-    Question(question_prompts[0], "a"),
-    Question(question_prompts[1], "b"),
-    Question(question_prompts[2], "a"),
-]
-
-
-def run_test(questions: list[Question]):
-    score = 0
-    for question in questions:
-        answer = input(question.prompt)
-        if answer == question.answer:
-            score += 1
-    print(f"Score: {str(score)} / {str(len(questions))}")
-    percentage = (score / len(questions)) * 100
-    if percentage >= 90:
-        print("A")
-    elif percentage >= 80:
-        print("B")
-    elif percentage >= 70:
-        print("C")
-    elif percentage >= 65:
-        print("D")
-    else:
-        print("F")
-
-
-run_test(questions)
+print(student1.getGpa())
+print(student2.getGpa())
